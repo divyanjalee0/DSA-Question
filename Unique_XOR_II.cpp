e7f2f446c811c63f7ec4867f0b0daa8683113c52
Divyanjalee Srivastav <srivastavdivyanjalee4@gmail.com>
2026-07-24 15:49:47 +0530

    Add files via upload

---
+++ Unique_XOR_II.cpp
@@ -0,0 +1,25 @@
+public class Unique_XOR_II {
+    
+ {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        //store all pair Xor
+        unordered_set<int> s1;
+        for(int i=0; i<n ; i++){
+            for(int j=i; j<n ; j++){
+                s1.insert(nums[i]^nums[j]);
+            }
+        }
+        //find triplet xor
+        unordered_set<int> s2;
+        for(int pairXor : s1){
+            for(int&num:nums){
+            s2.insert(pairXor^num);
+        }
+
+        }    
+        return s2.size();
+    }
+
+};
